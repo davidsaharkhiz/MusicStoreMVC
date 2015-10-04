4a8c69af9ee64252a1d171d5da9bbd2c63cbc6b2
davidsakh <davidsakh@gmail.com>
2015-10-04 17:15:38 -0400

    Groundwork for user preferences in session

---
+++ MvcMusicStore-Completed/MvcMusicStore/Controllers/PreferencesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class PreferencesController : Controller
+    {
+        // GET: Preferences
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+/*
+
+
+    todo
+
+    using System.Web
+
+public class Controller
+
+{
+
+public static Person person
+
+{
+
+ 
+get
+{
+    if(HttpContext.session["xyzperson"]==null)
+    {
+        Person p = new Person()
+        HttpContext.session["xyzperson"] = p;
+        return p;   
+    }
+    else
+    {
+        return (Person)HttpContext.session["xyzperson"];
+    }
+}
+
+} 
+
+} 
+
+
+    */
